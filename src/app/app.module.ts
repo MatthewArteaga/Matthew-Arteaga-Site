2e2aaf0b31ed8ec8be7292e40fd23fa29612c080
Matthew Arteaga <matthewarteaga@Matthews-MBP.attlocal.net>
2020-07-13 01:32:58 -0400

    page-body-and-footer-creation Added in new components for bootstrapping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './Components/page-header.component';
+import { PageFooterComponent } from './Components/page-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, PageHeaderComponent]
+  bootstrap: [
+    AppComponent, 
+    PageHeaderComponent, 
+    PageFooterComponent
+  ]
 })
 export class AppModule { }
