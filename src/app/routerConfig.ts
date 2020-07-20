6bdc40be9012e616e054ff592d0091457bd6c74c
Matthew Arteaga <matthewarteaga@Matthews-MBP.attlocal.net>
2020-07-19 22:26:45 -0400

    responsive-design Formatting corrections

--- src/app/routerConfig.ts
+++ src/app/routerConfig.ts
@@ -2,22 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home.component';
 import { AboutComponent } from './Components/about.component';
 import { WorkComponent } from './Components/work.component';
-// import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './Components/page-not-found.component';
 
 const appRoutes: Routes = [
-  { 
-    path: 'home', 
-    component: HomeComponent 
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'about',
     component: AboutComponent
   },
-  { path: 'work',
+  {
+    path: 'work',
     component: WorkComponent
   },
-    // Why doesn't this work?
   {
     path: '',
     redirectTo: '/home',
