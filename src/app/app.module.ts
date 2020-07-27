ed0d06551d6e36ef13599fb666a7defc72df3eab
Matthew Arteaga <matthewarteaga@Matthews-MBP.attlocal.net>
2020-07-27 01:39:09 -0400

    ngModule-implementation Removed uneeded component declarations.  Added in new page modules to imports array.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomeModule } from "./Modules/Home/home.module";
+import { AboutModule } from "./Modules/About/about.module";
+import { WorkModule } from "./Modules/Work/work.module";
+import { PageNotFoundModule } from "./Modules/404/page-not-found.module";
 
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './Components/page-header.component';
-import { PageFooterComponent } from './Components/page-footer.component';
-import { HomeComponent } from './Components/home.component';
-import { AboutComponent } from './Components/about.component';
-import { WorkComponent } from './Components/work.component';
-import { PageNotFoundComponent } from "./Components/page-not-found.component";
 
 import appRoutes from './routerConfig';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHeaderComponent,
-    PageFooterComponent,
-    HomeComponent,
-    AboutComponent,
-    WorkComponent,
-    PageNotFoundComponent
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
+    HomeModule,
+    AboutModule,
+    WorkModule,
+    PageNotFoundModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
