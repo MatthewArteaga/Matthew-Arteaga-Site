56ac5f939e34040d8c5093e9d1c5a57dc365aced
Matthew Arteaga <matthewarteaga@Matthews-MBP.attlocal.net>
2020-07-28 00:25:09 -0400

    animation-implementation Added in the three social media icons in a div container as a new social media bar component.  Corresponding template and css sheet were also created.

---
+++ src/app/Components/social-media-bar.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { RedirectService } from "../Services/redirect.service";
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+    selector: 'social-media',
+    templateUrl: '../Templates/social-media-bar.component.html',
+    styleUrls: ['../Styles/social-media-bar.component.css'],
+    animations: [
+        trigger('onOffLinkedIn', [
+            // ...
+            state('on', style({
+                height: '100px',
+                width: '100px',
+            })),
+            state('off', style({
+                height: '75px',
+                width: '75px',
+            })),
+            transition('on => off', [
+                animate('0.10s')
+            ]),
+            transition('off => on', [
+                animate('0.10s')
+            ]),
+        ]),
+        trigger('onOffGitHub', [
+            // ...
+            state('on', style({
+                height: '100px',
+                width: '100px',
+            })),
+            state('off', style({
+                height: '75px',
+                width: '75px',
+            })),
+            transition('on => off', [
+                animate('0.10s')
+            ]),
+            transition('off => on', [
+                animate('0.10s')
+            ]),
+        ]),
+        trigger('onOffInstagram', [
+            // ...
+            state('on', style({
+                height: '100px',
+                width: '100px',
+            })),
+            state('off', style({
+                height: '75px',
+                width: '75px',
+            })),
+            transition('on => off', [
+                animate('0.10s')
+            ]),
+            transition('off => on', [
+                animate('0.10s')
+            ]),
+        ]),
+    ]
+})
+export class SocialMediaBarComponent {
+    constructor(private externalRedirectCheck: RedirectService) { }
+
+    isOnLinkedIn = false;
+    isOnGitHub = false;
+    isOnInstagram = false;
+
+
+    toggle(button: string) {
+        if (button === 'LinkedIn') {
+            this.isOnLinkedIn = !this.isOnLinkedIn;
+        } else if (button === 'GitHub') {
+            this.isOnGitHub = !this.isOnGitHub;
+        } else {
+            this.isOnInstagram = !this.isOnInstagram;
+        }
+    }
+
+    onClick(link: string) {
+        this.externalRedirectCheck.navigate(link);
+    }
+}
