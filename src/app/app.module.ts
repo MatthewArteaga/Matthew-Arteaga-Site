3f145d9dd590ff82f0944a5013a451977e7b1f52
Matthew Arteaga <matthewarteaga@Matthews-MBP.attlocal.net>
2020-07-28 00:34:17 -0400

    animation-implementation Imported the browser animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeModule } from "./Modules/Home/home.module";
@@ -18,6 +19,7 @@ import appRoutes from './routerConfig';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HomeModule,
     AboutModule,
     WorkModule,
