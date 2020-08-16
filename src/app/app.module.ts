aba9b0f52768e86686097a8ab548100f9f1418d3
Matthew Arteaga <68165100+MatthewArteaga@users.noreply.github.com>
2020-08-16 03:23:18 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HomeModule } from "./Modules/Home/home.module";
-import { AboutModule } from "./Modules/About/about.module";
-import { WorkModule } from "./Modules/Work/work.module";
-import { PageNotFoundModule } from "./Modules/404/page-not-found.module";
-
-import { AppComponent } from './app.component';
-import { PageHeaderComponent } from './Components/page-header.component';
-
-import appRoutes from './routerConfig';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageHeaderComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HomeModule,
-    AboutModule,
-    WorkModule,
-    PageNotFoundModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [
